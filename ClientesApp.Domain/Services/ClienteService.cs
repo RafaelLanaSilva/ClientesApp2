86d96eadbcc7f6a93c9ee77242946a44ed771aa6
sergio-coti <cotiaulajava@outlook.com>
2024-10-21 12:49:54 -0300

    first commit

---
+++ ClientesApp.Domain/Services/ClienteService.cs
@@ -0,0 +1,41 @@
+﻿using ClientesApp.Domain.Dtos;
+using ClientesApp.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientesApp.Domain.Services
+{
+    /// <summary>
+    /// Implementação da interface de serviços de domínio
+    /// </summary>
+    public class ClienteService : IClienteService
+    {
+        public ClienteResponseDto Incluir(ClienteRequestDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClienteResponseDto Alterar(Guid id, ClienteRequestDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClienteResponseDto Excluir(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ClienteResponseDto> Consultar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClienteResponseDto ObterPorId(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
